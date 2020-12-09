a7fd92ffda32762464b4eb71a3b02f114b9a08d2
Arne <arne.otten90@googlemail.com>
2020-12-09 21:13:05 +0100

    Fixed abuse of reduce, used sum instead

--- 06/06_01.py
+++ 06/06_01.py
@@ -5,6 +5,6 @@ file = open(f"{os.path.dirname(os.path.abspath(__file__))}/input.txt", "r")
 input = file.read().split("\n\n")
 
 num = map(lambda l: len("".join(set(l.replace("\n","")))), input)
-num = reduce(lambda x,y: x+y, num)
+num = sum(num)
 
 print(f"Yes:{num}")
